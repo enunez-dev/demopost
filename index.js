756f647f7c8c422fe35b2a7d6a3113780c9f8321
edward.nunez <edward.nunez@bg.com.bo>
2021-04-12 14:14:16 -0400

    corrigiendo el puerto con heroku

--- index.js
+++ index.js
@@ -5,19 +5,20 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
-const port = 3050;
 
 routes.get('/', (req, res)=>{  
     res.json({message:'ok'});
-  });
+});
 
 routes.post('/', (req, res)=>{  
     const { body } = req;
     console.log(JSON.stringify(body));
     res.send('ok');
-  });
+});
 
 app.use(routes);
+
+const port = process.env.PORT || 3050;
 
 app.listen(port, ()=>{
     console.log(`Corriendo en el puerto:${port}` )
