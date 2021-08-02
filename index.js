a6919b4d1601065d41c2bd7e5fa2e7fbeecf28d0
edward.nunez <edward.nunez@bg.com.bo>
2021-08-02 14:15:27 -0400

    agregando demo

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const routes = express.Router();
 const app = express();
+const fs = require('fs');
+const { encrypt, decrypt } = require('./services/jweServices');
 
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
@@ -12,22 +14,33 @@ routes.get('/', (req, res)=>{
 
 routes.post('/', (req, res)=>{  
     const { body } = req;
-    console.log((body));
+    //console.log((body));
     res.send('ok');
 });
 routes.post('/v1', (req, res)=>{  
     const { body } = req;
-    console.log('v1-----------------------------------------');
-    console.log((body));
+    //console.log('v1-----------------------------------------');
+    //console.log((body));
     res.send('ok');
 });
 
 routes.post('/v2', (req, res)=>{  
     const { body } = req;
-    console.log('v2-----------------------------------------');
-    console.log((body));
+    //console.log('v2-----------------------------------------');
+    //console.log((body));
     res.send('ok');
 });
+
+routes.post('/demo',async (req, res)=>{
+    const { body } = req;
+    console.log('Body-----------------------------------------');
+    console.log(body);
+    const pemPrivate = fs.readFileSync('./assets/pemPrivate.txt', 'utf8');
+    const result = await decrypt(pemPrivate, body);
+    console.log('Result-----------------------------------------');
+    console.log(result)
+    res.send('ok');
+})
 
 app.use(routes);
 
